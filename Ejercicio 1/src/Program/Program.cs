572b65e6ac95406dd281602d0efc3a80d7b129a0
Ignacio Carbajal <ignaciocarbajal2002@gmail.com>
2023-08-28 10:06:07 -0300

    comentarios prueba

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -10,8 +10,9 @@ namespace SRP
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
             book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            //book2.ShelveBook("B","3");
 
         }
     }
 }
+
